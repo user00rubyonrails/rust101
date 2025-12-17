68c05c0b51da6d14b76372147519f0f7e9ad9216
lion <user00rubyonrails@gmail.com>
2025-12-17 22:51:05 +0700

    add note

--- chap3/manager_views/src/views/auth/mod.rs
+++ chap3/manager_views/src/views/auth/mod.rs
@@ -4,12 +4,19 @@ mod logout;
 use super::path::Path;
 
 pub fn auth_factory(app: &mut web::ServiceConfig, logout: bool) {
-    let base_path: Path = Path {prefix: "/auth".to_string()};
+    let base_path: Path = Path {
+        prefix: "/auth".to_string(),
+    };
 
-    let app = app.route(&base_path.define(String::from("/login")), web::get().to(login::login));
+    let app = app.route(
+        &base_path.define(String::from("/login")),
+        web::get().to(login::login),
+    );
 
     if logout {
-        app.route(&base_path.define(String::from("logout")), web::get().to(logout::logout));
+        app.route(
+            &base_path.define(String::from("logout")),
+            web::get().to(logout::logout),
+        );
     }
-
 }
