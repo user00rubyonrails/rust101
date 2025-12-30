7091162242245c56c41ef803736eb881f09d64f7
lion <user00rubyonrails@gmail.com>
2025-12-30 23:28:09 +0700

    Create /login endpoint, next in login html

--- chap3/manager_views/src/json_serialization/mod.rs
+++ chap3/manager_views/src/json_serialization/mod.rs
@@ -1,3 +1,4 @@
 pub mod to_to_items;
 pub mod to_to_item;
 pub mod new_user;
+pub mod login;
--- chap3/manager_views/src/views/auth/login.rs
+++ chap3/manager_views/src/views/auth/login.rs
@@ -1,3 +1,27 @@
-pub async fn login() -> String {
-    format!("Login view")
+use actix_web::{HttpResponse, web};
+use diesel::prelude::*;
+use crate::{auth::jwt::JwtToken, database::establish_connection, json_serialization::login::Login, models::user::user::User, schema::users};
+
+pub async fn login(credentials: web::Json<Login>) -> HttpResponse {
+    println!("[LOG] pub async fn login");
+    let username = credentials.username.clone();
+    let password = credentials.password.clone();
+
+    let conn = establish_connection();
+    let users = users::table
+        .filter(users::columns::username.eq(username.as_str()))
+        .load::<User>(&conn).unwrap();
+    if users.len() == 0 { // no user in db: notfound!
+        return HttpResponse::NotFound().await.unwrap()
+    } else if users.len() > 1 { // have more than 1 user in db: conflict!!
+        return HttpResponse::Conflict().await.unwrap()
+    }
+
+    match users[0].clone().verify(password) { // get the first index 0 user and return `token` header
+        true => {
+            let token = JwtToken::encode(users[0].clone().id);
+            HttpResponse::Ok().header("token", token).await.unwrap()
+        },
+        false => HttpResponse::Unauthorized().await.unwrap()
+    }
 }
