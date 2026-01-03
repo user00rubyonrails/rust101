5a477c58b49710056f7d3068682d36a6e12b7205
lion <user00rubyonrails@gmail.com>
2026-01-03 23:28:42 +0700

    Adding state to our web app

---
+++ message-actix/note.md
@@ -0,0 +1,59 @@
+## handle error by Result<T, E>
+struct Result {
+  Ok(T),
+  Err(E),
+}
+
+### Can check it bye use: `match` 
+```rust
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt"); // Result<Ok(T), Err(E)>
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file, // Ok(T)
+        Err(error) => match error.kind() { // Err(E) - Result<Ok(T), Err(E)>
+            ErrorKind::NotFound => match File::create("hello.txt") { // Ok(T) - Result<Ok(T), Err(E)>
+                Ok(fc) => fc, Ok(T)
+                Err(e) => panic!("Problem creating the file: {e:?}"), // Err(E)
+            },
+            _ => { // Err(E)
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
+    };
+}
+```
+### Reduce `match` by use closure |error| {} and `unwrap_or_else()` method
+```rust
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let greeting_file = match File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {e:?}")
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
+}
+```
+
+### shortcut of panic on Error: 
+.unwrap()
+.expect("custom msg"): In production-quality code, most Rustaceans choose expect rather than unwrap and give more context about why the operation is expected to always succeed. 
+```rust
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let greeting_file = match File::open("hello.txt").unwrap();
+    let greeting_file_2 = match File::open("hello.txt").expect("cannot open file");
+}
+```
+
--- message-actix/src/lib.rs
+++ message-actix/src/lib.rs
@@ -1,8 +1,15 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{App, HttpRequest, HttpServer, Result, middleware, web};
-use serde::Serialize;
+use std::{
+    cell::Cell,
+    sync::{Arc, Mutex, atomic::AtomicUsize},
+};
+
+use actix_web::{App, HttpServer, Result, middleware, web};
+
+// use `Serde` to turn JSON data with that format into instances of our struct
+use serde::{Deserialize, Serialize};
 
 pub struct MessageApp {
     port: u16,
@@ -14,11 +21,24 @@ impl MessageApp {
     }
 
     pub fn run(&self) -> std::io::Result<()> {
+        let messages = Arc::new(Mutex::new(vec![]));
         println!("Starting http server: 127.0.0.1:{}", self.port);
+
         HttpServer::new(move || {
             App::new()
+                .data(AppState {
+                    server_id: SERVER_COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone(),
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(
+                    web::resource("/send")
+                        .data(web::JsonConfig::default().limit(4096))
+                        .route(web::post().to(post)),
+                )
+                .service(clear)
         })
         .bind(("127.0.0.1", self.port))? // ? operator return Result<Ok, Err> - if return Result<Err> return early with that value
         .workers(8)
@@ -28,18 +48,66 @@ impl MessageApp {
 
 #[derive(Serialize)]
 struct IndexResponse {
-    message: String,
+    server_id: usize,
+    request_count: usize,
+    message: Vec<String>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req
-        .headers()
-        .get("hello_world_key")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
 
     Ok(web::Json(IndexResponse {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        message: ms.clone(),
+    }))
+}
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
+}
+
+#[derive(Deserialize)]
+struct PostInput {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
+    message: String,
+}
+
+fn post(msg: web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+#[post("/clear")]
+fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexResponse {
+        server_id: state.server_id,
+        request_count,
+        message: vec![],
     }))
 }
